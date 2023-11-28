058425dceb823c1f35d48a301232a9c24ea7f70f
jehadmelad <jd.melad@gmail.com>
2023-11-28 09:22:21 +0100

    ✨  yw_pipeline.py => change application icon and style.

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -2,6 +2,17 @@ from youwol.environment import YouwolEnvironment
 from youwol.routers.projects import IPipelineFactory, BrowserApp, Execution, Link, BrowserAppGraphics, OpenWith
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
 from youwol_utils.context import Context
+from pathlib import Path
+
+from youwol.app.environment import YouwolEnvironment
+from youwol.app.routers.projects import IPipelineFactory, BrowserApp, Execution, Link, BrowserAppGraphics, OpenWith
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, PublishConfig
+from youwol.utils.context import Context
+
+folder_path = Path(__file__).parent.parent
+pkg_json = parse_json(folder_path / "package.json")
+asset_id = encode_id(pkg_json['name'])
+version = pkg_json['version']
 
 
 class PipelineFactory(IPipelineFactory):
@@ -22,8 +33,8 @@ class PipelineFactory(IPipelineFactory):
                 ]
             ),
             graphics=BrowserAppGraphics(
-                appIcon={'class': 'fas fa-cubes fa-2x'},
-                fileIcon={}
+                appIcon=icon(size_px='100%', border_radius='15%', icon_path=app_icon),
+                fileIcon=icon(size_px='100%', border_radius='15%', icon_path=file_icon, bg_size='contain'),
             ),
             links=[
                 Link(name="doc", url="dist/docs/index.html"),
@@ -32,3 +43,23 @@ class PipelineFactory(IPipelineFactory):
             ]
         ))
         return await pipeline(config, context)
+
+
+assets_dir = f"/api/assets-gateway/cdn-backend/resources/{asset_id}/{version}/assets"
+app_icon = f"url('{assets_dir}/potree_viewer_app.svg')"
+file_icon = f"url('{assets_dir}/potree_viewer_file.svg')"
+
+
+def icon(size_px: str, border_radius: str, icon_path: str, bg_size: str = "cover"):
+    return {
+        "style": {
+            "width": f"{size_px}",
+            "height": f"{size_px}",
+            "background-image": icon_path,
+            "background-size": bg_size,
+            "background-repeat": "no-repeat",
+            "background-position": "center center",
+            "filter": "drop-shadow(rgb(0, 0, 0) 1px 3px 5px)",
+            "border-radius": f"{border_radius}",
+        }
+    }
